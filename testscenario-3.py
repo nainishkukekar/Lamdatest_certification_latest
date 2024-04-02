958220888fc53ea12bf63d661559bf5296e1df03
Nainish k <nainishk05@gmail.com>
2024-04-02 12:45:32 +0530

    lamdatest test scenarios

--- testscenario-3.py
+++ testscenario-3.py
@@ -10,9 +10,9 @@ capabilities_windows10_chrome_latest = {
     'LT:Options': {
         'platform': 'Windows 10',
         'build': 'Playwright Python Build-1',
-        'name': 'Test Scenario-1 (Windows 10 Chrome-Latest)',
-        'user': 'nainishk05',
-        'accessKey':'7cvBWS7EO37gEW9xYmvgpQZA7z8VTGQybQP2hU6sUOg1tniDTE',
+        'name': 'Test Scenario-3 (Windows 10 Chrome-Latest)',
+        'user': 'nainishkukekar',
+        'accessKey': 'bNutSgMdjh2YsQnTW4KuJ65SqG7H1OFReBWlpGnLiUInUzF0Vr',
         'network': True,
         'video': True,
         'headless': False,
@@ -30,9 +30,9 @@ capabilities_windows11_chrome_l20 = {
     'LT:Options': {
         'platform': 'Windows 11',
         'build': 'Playwright Python Build-1',
-        'name': 'Test Scenario-1 (Windows 11 Chrome-120)',
-        'user': 'nainishk05',
-        'accessKey':'7cvBWS7EO37gEW9xYmvgpQZA7z8VTGQybQP2hU6sUOg1tniDTE',
+        'name': 'Test Scenario-3 (Windows 11 Chrome-120)',
+        'user': 'nainishkukekar',
+        'accessKey': 'bNutSgMdjh2YsQnTW4KuJ65SqG7H1OFReBWlpGnLiUInUzF0Vr',
         'network': True,
         'video': True,
         'headless': False,
@@ -43,7 +43,6 @@ capabilities_windows11_chrome_l20 = {
     }
 }
 
-
 def run_test(capabilities):
     with sync_playwright() as playwright:
         playwrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
@@ -53,18 +52,29 @@ def run_test(capabilities):
         page = browser.new_page()
         try:
             page.goto("https://www.lambdatest.com/selenium-playground")
-            page.click("//a[normalize-space()='Simple Form Demo']")
+            page.click("//a[normalize-space()='Input Form Submit']")
             page.wait_for_timeout(1000)
-            title = page.title()
-            print("Title:: ", title)
-            if "Selenium" in title:
-                set_test_status(page, "passed", "Title matched")
+            page.fill("#name", "John Doe")
+            page.fill("#inputEmail4", "john.doe@example.com")
+            page.fill("#inputPassword4", "1234567890")
+            page.fill("#company", "123 Main St")
+            page.fill("#websitename", "New York")
+            page.fill("#inputCity", "New York")
+            page.fill("#inputAddress1", "123 Main St")
+            page.fill("#inputAddress2", "Apt 101")
+            page.fill("#inputState", "New York")
+            page.fill("#inputZip", "10001")
+            page.select_option('select[name="country"]', label="United States")
+            print("Form filled successfully.")
+            page.click("//button[normalize-space()='Submit']")
+            page.wait_for_timeout(5000)
+            paragraph_element = page.query_selector('p.success-msg')
+            inner_text = paragraph_element.text_content()
+            expected_text = "Thanks for contacting us, we will get back to you shortly."
+            if inner_text.strip() == expected_text:
+                set_test_status(page, "passed", "Success messsage validation passed ")
             else:
-                set_test_status(page, "failed", "Title did not match")
-            welcome_message = "Welcome to LambdaTest"
-            page.fill("//input[@id='user-message']", welcome_message)
-            page.click("//*[@id='showInput']")
-            assert page.text_content("//p[@id='message']") == welcome_message
+                set_test_status(page, "failed", "Success messsage validation Failed")
         except Exception as err:
             print("Error:: ", err)
             set_test_status(page, "failed", str(err))
